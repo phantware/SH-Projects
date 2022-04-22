139da32bcbafcfab4a47ba93720e2855e4e64a58
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-22 16:50:37 +0100

    added dynamic button for passing props

--- front-end-path/lesson2/src/components/About.jsx
+++ front-end-path/lesson2/src/components/About.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div>
+      About
+      <Button props={'lala'} />
+      <Button props={'motunde'} />
+    </div>
+  )
 }
 
 export default About
