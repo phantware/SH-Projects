6f8859e506f36ae45c9bbf2c567f67a8b1da0f00
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-22 12:00:02 +0100

    class on react-router-dom

--- front-end-path/lesson2/src/App.js
+++ front-end-path/lesson2/src/App.js
@@ -1,7 +1,32 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
-  return <div className='App'>hello app</div>
+  return (
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home />}></Route>
+        <Route exact path='/about' element={<About />}></Route>
+        <Route exact path='/contact' element={<Contact />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
