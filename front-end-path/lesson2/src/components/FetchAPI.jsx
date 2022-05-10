561c97149c90a0815e2b2c43df112c833d68ab9c
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-10 13:48:21 +0100

    fetching data from json place holder using fetch api

---
+++ front-end-path/lesson2/src/components/FetchAPI.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FetchAPI = () => {
+  const fetchPost = async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const response = await data.json()
+    console.log(response)
+  }
+  fetchPost()
+  return (
+    <div>
+      <h1>Using Fetch API to fetch API</h1>
+    </div>
+  )
+}
+
+export default FetchAPI
--- front-end-path/lesson2/src/components/Home.jsx
+++ front-end-path/lesson2/src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import FetchAPI from './FetchAPI'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <FetchAPI />
+    </div>
+  )
 }
 
 export default Home
