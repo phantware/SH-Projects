6bd1b45565456704e99a61c5494744ebbe4d1b0c
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-11 16:33:25 +0100

    ejs installed and required

--- back-end-path/lesson2-backend/app.js
+++ back-end-path/lesson2-backend/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-const port = 5000
+app.use(express.static(path.join(__dirname, 'public/')))
 
-app.set('views')
+app.set('views', path.join(__dirname, 'src/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   return res.send('Hello World')
 })
 
-app.listen(port, () => console.log(`app running on port $port`))
+const port = 5000
+
+app.listen(port, () => console.log(`app running on port ${port}`))
