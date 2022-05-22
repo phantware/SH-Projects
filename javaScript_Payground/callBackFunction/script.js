c128be1411204acf75fadda5f5e59b0796eb6b4e
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-22 14:08:15 +0100

    eventListener

--- javaScript_Payground/callBackFunction/script.js
+++ javaScript_Payground/callBackFunction/script.js
@@ -1,13 +1,22 @@
+//Click Handler
+
+let count = 0
+const getBtn = document.getElementById('clickMe')
+getBtn.addEventListener('click', () => {
+  // alert('Hello World')
+  console.log('hello world', count++)
+})
+
 //CallBack Explained
 
-setTimeout(() => {
-  console.log('Timer')
-}, 5000)
+// setTimeout(() => {
+//   console.log('Timer')
+// }, 5000)
 
-function x(y) {
-  console.log('x')
-  y()
-}
-x(function y() {
-  console.log('y')
-})
+// function x(y) {
+//   console.log('x')
+//   y()
+// }
+// x(function y() {
+//   console.log('y')
+// })
