66c76a5006a01b7c711b6902683e20eff059f7bf
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-22 15:57:17 +0100

    filtering array using filter

--- javaScript_Payground/callBackFunction/script.js
+++ javaScript_Payground/callBackFunction/script.js
@@ -1,11 +1,11 @@
 //Click Handler
 
-let count = 0
-const getBtn = document.getElementById('clickMe')
-getBtn.addEventListener('click', () => {
-  // alert('Hello World')
-  console.log('hello world', count++)
-})
+// let count = 0
+// const getBtn = document.getElementById('clickMe')
+// getBtn.addEventListener('click', () => {
+//   // alert('Hello World')
+//   console.log('hello world', count++)
+// })
 
 //CallBack Explained
 
@@ -20,3 +20,13 @@ getBtn.addEventListener('click', () => {
 // x(function y() {
 //   console.log('y')
 // })
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const empty = []
+
+const result = arr.filter((arr2) => {
+  return arr2 % 2 == 0
+})
+empty.push(result)
+console.log('result', result)
+console.log('empty', empty)
