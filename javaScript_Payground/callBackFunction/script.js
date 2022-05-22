4a1eef5374f269ca3a65b5e923a5fd6b9ffa77ab
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-22 16:12:06 +0100

    using while loop for even number

--- javaScript_Payground/callBackFunction/script.js
+++ javaScript_Payground/callBackFunction/script.js
@@ -21,12 +21,26 @@
 //   console.log('y')
 // })
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const empty = []
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const empty = []
 
-const result = arr.filter((arr2) => {
-  return arr2 % 2 == 0
-})
-empty.push(result)
-console.log('result', result)
-console.log('empty', empty)
+// const result = arr.filter((arr2) => {
+//   return arr2 % 2 == 0
+// })
+// empty.push(result)
+// console.log('result', result)
+// console.log('empty', empty)
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function getEven(arr, n = 0) {
+  const evenList = []
+  while (n < arr.length) {
+    if (arr[n] % 2 === 0) {
+      evenList.push(arr[n])
+    }
+    n++
+  }
+  return evenList
+}
+console.log(getEven(list))
