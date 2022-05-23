73b46bade6f51681aec766bb9fa8649a77463bf5
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-23 14:59:48 +0100

    javascript code added

---
+++ javaScript_Payground/calculator/script.js
@@ -0,0 +1,67 @@
+const state = {
+  result: 0,
+  history: [],
+  currentOperation: {
+    '1stOperands': '',
+    '2ndOperands': '',
+    operator: '',
+  },
+}
+
+const renderResult = () => {
+  document.querySelector('.screen-result').innerHTML = state.result
+}
+const renderHistory = () => {
+  document.querySelector('.screen-history').innerHTML = state.history
+    .map((historyItem) => `<div class="screen-operation">${historyItem}</div>`)
+    .join('')
+}
+const render1stCurrentOperationOperand = () => {
+  document.querySelector('.screen-current-operation-operands-1st').innerHTML =
+    state.currentOperation['1stOperands'] // get from state
+}
+const render2ndCurrentOperationOperand = () => {
+  document.querySelector('.screen-current-operation-operands-2nd').innerHTML =
+    state.currentOperation['2ndOperands']
+}
+const renderCurrentOperationOperator = () => {
+  document.querySelector('.screen-current-operation-operator').innerHTML =
+    state.currentOperation.operator // get from state
+}
+
+renderResult()
+
+document.querySelectorAll('.operand').forEach((operand_button) => {
+  operand_button.addEventListener('click', () => {
+    if (state.currentOperation.operator !== '') {
+      state.currentOperation['2ndOperands'] += operand_button.textContent // set to state
+      render2ndCurrentOperationOperand()
+    } else {
+      state.currentOperation['1stOperands'] += operand_button.textContent // set to state
+      render1stCurrentOperationOperand()
+    }
+
+    console.log(state.currentOperation.operator)
+  })
+})
+
+document.querySelectorAll('.operator').forEach((operator_button) => {
+  operator_button.addEventListener('click', () => {
+    state.currentOperation.operator = operator_button.textContent // set to state
+    renderCurrentOperationOperator()
+  })
+})
+
+document.querySelector('.equals').addEventListener('click', () => {
+  if (state.currentOperation.operator == '+') {
+    state.result =
+      Number(state.currentOperation['1stOperands']) +
+      Number(state.currentOperation['2ndOperands']) // set to state
+
+    const operation = `${state.currentOperation['1stOperands']} ${state.currentOperation.operator} ${state.currentOperation['2ndOperands']} = ${state.result}`
+    state.history.push(operation) // set to state
+  }
+
+  renderResult()
+  renderHistory()
+})
