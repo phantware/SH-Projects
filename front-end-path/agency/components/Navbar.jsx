8bc713bbba39c200d5cd7222b738da5a59ca63c6
phantware <phantmoney2011@gmail.com>
2022-07-18 15:58:15 +0100

    navbar designed

--- front-end-path/agency/components/Navbar.jsx
+++ front-end-path/agency/components/Navbar.jsx
@@ -1,5 +1,28 @@
+import styles from '../styles/Navbar.module.css'
+import Link from 'next/link'
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className={styles.container}>
+      <Link href='/'>AGENCY</Link>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <Link href='/products/design'>DESIGN</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href='/products/development '>DEVELOPMENT</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href='/products/production'>PRODUCTION</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href='/products/photography'>PHOTOGRAPHY</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href='/products/contact'>CONTACT</Link>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Navbar
