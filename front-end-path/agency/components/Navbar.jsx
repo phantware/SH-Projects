02bc4d6cc2e5c0d457311914133cf23147fbf383
phantware <phantmoney2011@gmail.com>
2022-07-24 10:34:59 +0100

    menu bar created

--- front-end-path/agency/components/Navbar.jsx
+++ front-end-path/agency/components/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className={styles.container}>
       <Link href='/'>AGENCY</Link>
@@ -9,7 +13,7 @@ const Navbar = () => {
           <Link href='/products/design'>DESIGN</Link>
         </li>
         <li className={styles.listItem}>
-          <Link href='/products/development '>DEVELOPMENT</Link>
+          <Link href='/products/development'>DEVELOPMENT</Link>
         </li>
         <li className={styles.listItem}>
           <Link href='/products/production'>PRODUCTION</Link>
@@ -18,6 +22,31 @@ const Navbar = () => {
           <Link href='/products/photography'>PHOTOGRAPHY</Link>
         </li>
         <li className={styles.listItem}>
+          <Link href='/contact'>CONTACT</Link>
+        </li>
+      </ul>
+      <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+        <div className={styles.line} />
+        <div className={styles.line} />
+        <div className={styles.line} />
+      </div>
+      <ul className={styles.menu} style={{ right: open ? '0px' : '-50vw' }}>
+        <li className={styles.menuItem}>
+          <Link href='/'>HOME</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/products/design'>DESIGN</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/products/development'>DEVELOPMENT</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/products/production'>PRODUCTION</Link>
+        </li>
+        <li className={styles.menuItem}>
+          <Link href='/products/photography'>PHOTOGRAPHY</Link>
+        </li>
+        <li className={styles.menuItem}>
           <Link href='/contact'>CONTACT</Link>
         </li>
       </ul>
