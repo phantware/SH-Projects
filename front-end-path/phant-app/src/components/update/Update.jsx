ed6c653ca48d700e37827d256aa9d40461dbaf4b
phantware <phantmoney2011@gmail.com>
2022-08-01 17:23:13 +0100

    remove dispatch aded

--- front-end-path/phant-app/src/components/update/Update.jsx
+++ front-end-path/phant-app/src/components/update/Update.jsx
@@ -3,7 +3,7 @@ import Warning from '../warning/Warning'
 import './update.css'
 import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { update } from '../../redux/userSlice'
+import { update, remove } from '../../redux/userSlice'
 
 export default function Update() {
   const [name, setName] = useState('')
@@ -16,12 +16,19 @@ export default function Update() {
     dispatch(update({ name, email }))
   }
 
+  const handleDelete = (e) => {
+    e.preventDefault()
+    dispatch(remove())
+  }
+
   return (
     <div className='update'>
       <div className='updateWrapper'>
         <h3 className='updateTitle'>Update Your Account</h3>
         <Warning />
-        <button className='delete'>Delete Account</button>
+        <button className='delete' onClick={handleDelete}>
+          Delete Account
+        </button>
         <div className='updateContainer'>
           <form>
             <div className='formItem'>
--- front-end-path/phant-app/src/redux/userSlice.js
+++ front-end-path/phant-app/src/redux/userSlice.js
@@ -13,9 +13,10 @@ const userSlice = createSlice({
       state.name = action.payload.name
       state.email = action.payload.email
     },
+    remove: (state) => (state = {}),
   },
 })
 
-export const { update } = userSlice.actions
+export const { update, remove } = userSlice.actions
 
 export default userSlice.reducer
