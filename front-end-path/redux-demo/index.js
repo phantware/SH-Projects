d610a4e37962f299215305807db1b239cba67606
phantware <phantmoney2011@gmail.com>
2022-08-15 23:22:45 +0100

    store implemented

--- front-end-path/redux-demo/index.js
+++ front-end-path/redux-demo/index.js
@@ -1,3 +1,6 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const BUY_CAKE = 'BUY_CAKE'
 
 // THIS IS OUR ACTION
@@ -38,3 +41,13 @@ const reducer = (state = initialState, action) => {
       return state
   }
 }
+
+const store = createStore(reducer)
+console.log('initialState', store.getState())
+const unsubscribe = store.subscribe(() =>
+  console.log('updated state', store.getState())
+)
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
