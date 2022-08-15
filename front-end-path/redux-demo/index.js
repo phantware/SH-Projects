75ebd62cf517ec2232503f5cc8089a3210be27ca
phantware <phantmoney2011@gmail.com>
2022-08-15 22:22:34 +0100

    first redux action

--- front-end-path/redux-demo/index.js
+++ front-end-path/redux-demo/index.js
@@ -1,1 +1,19 @@
-console.log('from index.js')
+const BUY_CAKE = 'BUY_CAKE'
+
+// THIS IS OUR ACTION
+/**
+ * 
+ {
+   type: BUY_CAKE
+   info: "First redux action"
+ }
+ */
+
+// THIS IS AN ACTION CREATOR
+
+const buyCake = () => {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action',
+  }
+}
