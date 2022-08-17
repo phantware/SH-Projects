7fbbfa98978667aed6f43873b3455543c7692e6f
phantware <phantmoney2011@gmail.com>
2022-08-17 23:37:50 +0100

    useDispatch used instead of mapDispatchToProps

--- front-end-path/redux-demo/react-redux/src/components/HooksCakeContainer.js
+++ front-end-path/redux-demo/react-redux/src/components/HooksCakeContainer.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { buyCake } from '../redux'
 
 const HooksCakeContainer = () => {
   const numOfCake = useSelector((state) => state.numOfCake)
+  const dispatch = useDispatch()
   return (
     <div>
       <h1>HooksCakeContainer</h1>
       <h2>Number of Cake - {numOfCake}</h2>
-      <button>Buy Cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake</button>
     </div>
   )
 }
