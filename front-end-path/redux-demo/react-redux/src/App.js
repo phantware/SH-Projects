eac0e1513f718649a447a7e9c74532e9d06d40ae
phantware <phantmoney2011@gmail.com>
2022-08-18 17:17:21 +0100

    usercontainer updated

--- front-end-path/redux-demo/react-redux/src/App.js
+++ front-end-path/redux-demo/react-redux/src/App.js
@@ -1,18 +1,20 @@
 import { Provider } from 'react-redux'
 import './App.css'
-import CakeContainer from './components/CakeContainer'
-import HooksCakeContainer from './components/HooksCakeContainer'
-import IceCreamContainer from './components/iceCreamContainer'
-import NewCakeContainer from './components/NewCakeContainer'
+// import CakeContainer from './components/CakeContainer'
+// import HooksCakeContainer from './components/HooksCakeContainer'
+// import IceCreamContainer from './components/iceCreamContainer'
+// import NewCakeContainer from './components/NewCakeContainer'
+import UserContainer from './components/UserContainer'
 import store from './redux/store'
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
-        <CakeContainer />
+        {/* <CakeContainer />
         <HooksCakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
+        <UserContainer />
       </div>
     </Provider>
   )
