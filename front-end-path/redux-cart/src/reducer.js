07985aaf22384cf240e1853c6d12189168d012cd
phantware <phantmoney2011@gmail.com>
2022-08-19 19:00:47 +0100

    if statement used in reducer instead of switch

--- front-end-path/redux-cart/src/reducer.js
+++ front-end-path/redux-cart/src/reducer.js
@@ -1,16 +1,28 @@
-import { DECREASE, INCREASE, CLEAR_CART } from './actions'
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE } from './actions'
 
 const reducer = (state, action) => {
-  switch (action.type) {
-    case CLEAR_CART: {
-      return {
-        ...state,
-        cart: [],
-      }
-    }
-    default:
-      return state
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] }
+  }
+  if (action.type === DECREASE) {
+    console.log('you decreased amount')
+  }
+  if (action.type === INCREASE) {
+    console.log('you increased amount')
+  }
+  if (action.type === REMOVE) {
+    console.log('you removed amount')
   }
 }
-
 export default reducer
+
+// switch (action.type) {
+//   case CLEAR_CART: {
+//     return {
+//       ...state,
+//       cart: [],
+//     }
+//   }
+//   default:
+//     return state
+// }
