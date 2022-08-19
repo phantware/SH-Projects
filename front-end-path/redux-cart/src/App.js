61cf5db0ebf1452590d6642fe66b84384c9c80e2
phantware <phantmoney2011@gmail.com>
2022-08-19 12:30:19 +0100

    redux added

--- front-end-path/redux-cart/src/App.js
+++ front-end-path/redux-cart/src/App.js
@@ -4,12 +4,26 @@ import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 // items
 import cartItems from './cart-items'
+
 // redux stuff
-// Store - stores data, think of state
+
 // reducer - funcion that used to update
+//reducer - It takes two arguments (state,action)
+//state - old state/state before update
+//action - what happened/ what update
+// reducer - return updated or old state
+// Store - stores data, think of state
 
 import { createStore } from 'redux'
-const reducer = () => {}
+
+const initialState = {
+  count: 0,
+}
+
+const reducer = (state = initialState, action) => {
+  console.log({ state, action })
+  return state
+}
 const store = createStore(reducer)
 
 function App() {
