9393544ef9c6b7c35d429bc6a8f579daf90eac84
phantware <phantmoney2011@gmail.com>
2022-11-02 15:42:41 +0100

    post component info added

--- front-end-path/loadingScreens/client/src/components/post/Post.jsx
+++ front-end-path/loadingScreens/client/src/components/post/Post.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 const Post = () => {
-  return <div>Post</div>
+  return (
+    <div className='post'>
+      <div className='postImgContainer'>
+        <img
+          src='https://i.ibb.co/KDtnJVY/logo.png'
+          alt=''
+          className='postImg'
+        />
+        <span className='postDuration'>10:30</span>
+      </div>
+      <div className='postInfoContainer'>
+        <img
+          className='postAvatar'
+          src='https://i.ibb.co/KDtnJVY/logo.png'
+          alt=''
+        />
+        <div className='postInfo'>
+          <span className='postTitle'>Video Title</span>
+          <span className='postChannel'>Video name</span>
+          <span className='postDetail'>5k views • 11/02/2022</span>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Post
