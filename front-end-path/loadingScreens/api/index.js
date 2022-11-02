909f5b266d20d88ca33b586ac2312757064bf23c
phantware <phantmoney2011@gmail.com>
2022-11-02 16:08:32 +0100

    cors resolution added to the backend

--- front-end-path/loadingScreens/api/index.js
+++ front-end-path/loadingScreens/api/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const cors = require('cors')
+
 const { videos, user } = require('./fakeData')
 const app = express()
+
+// const bodyParser = require('body-parser')
+
+const corsOption = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+// Middlewares
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.get('/api/user/1', (req, res) => {
