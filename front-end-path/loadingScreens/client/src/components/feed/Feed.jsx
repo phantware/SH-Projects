8b45f772cb4274925fc8a2fe5980139beb7dec1e
phantware <phantmoney2011@gmail.com>
2022-11-06 17:16:43 +0100

    video props passed in Post component

--- front-end-path/loadingScreens/client/src/components/feed/Feed.jsx
+++ front-end-path/loadingScreens/client/src/components/feed/Feed.jsx
@@ -1,18 +1,31 @@
 import './feed.css'
 import Post from '../post/Post'
+import axiosInstance from '../../utils/axiosInstance'
+import { useState, useEffect } from 'react'
 
 const Feed = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const getVideos = async () => {
+      setIsLoading(true)
+      try {
+        const res = await axiosInstance.get('/api/videos/1')
+        setVideos(res.data)
+      } catch (err) {
+        setIsLoading(false)
+      }
+    }
+    getVideos()
+  }, [])
+
+  console.log('videos', videos)
   return (
     <div className='feed'>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {videos.map((video) => (
+        <Post video={video} />
+      ))}
     </div>
   )
 }
