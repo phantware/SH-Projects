5d84a67b5a7e825fdd294f2de708473aac311270
phantware <phantmoney2011@gmail.com>
2022-11-12 12:48:52 +0100

    creating a list of products

--- front-end-path/city-market/pages/index.js
+++ front-end-path/city-market/pages/index.js
@@ -1,5 +1,16 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
+import data from '../utils/data'
 
 export default function Home() {
   return (
@@ -7,11 +18,30 @@ export default function Home() {
       <div>
         <div>
           <h1>Products</h1>
-          <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-          </ul>
+          <Grid container spacing={3}>
+            {data.products.map((product) => (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size='small' color='primary'>
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </div>
       </div>
     </Layout>
