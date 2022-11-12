efeed450ef1f8d6e75c9ece5b3ba45ecf277b2d5
phantware <phantmoney2011@gmail.com>
2022-11-12 16:45:33 +0100

    product folder created with slug file

--- front-end-path/city-market/pages/index.js
+++ front-end-path/city-market/pages/index.js
@@ -11,6 +11,7 @@ import {
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 import data from '../utils/data'
+import NextLink from 'next/link'
 
 export default function Home() {
   return (
@@ -22,16 +23,18 @@ export default function Home() {
             {data.products.map((product) => (
               <Grid item md={4} key={product.name}>
                 <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component='img'
-                      image={product.image}
-                      title={product.name}
-                    ></CardMedia>
-                    <CardContent>
-                      <Typography>{product.name}</Typography>
-                    </CardContent>
-                  </CardActionArea>
+                  <NextLink href={`/product/${product.slug}`} passHref>
+                    <CardActionArea>
+                      <CardMedia
+                        component='img'
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </NextLink>
                   <CardActions>
                     <Typography>${product.price}</Typography>
                     <Button size='small' color='primary'>
