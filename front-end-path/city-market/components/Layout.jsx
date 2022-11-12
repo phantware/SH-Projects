9b4205e6f25adc962102fde61ddc68c5d39bc2cb
phantware <phantmoney2011@gmail.com>
2022-11-12 07:21:10 +0100

    material-ui styles used

--- front-end-path/city-market/components/Layout.jsx
+++ front-end-path/city-market/components/Layout.jsx
@@ -1,19 +1,24 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core'
 import Head from 'next/head'
 import React from 'react'
+import useStyles from '../utils/styles'
 
 const Layout = ({ children }) => {
+  const classes = useStyles()
   return (
     <div>
       <Head>
         <title>City Market</title>
       </Head>
-      <AppBar position='static'>
+      <AppBar position='static' className={classes.navbar}>
         <Toolbar>
           <Typography>City Market</Typography>
         </Toolbar>
       </AppBar>
       <Container>{children}</Container>
+      <footer>
+        <Typography>All rights reserved. City Market.</Typography>
+      </footer>
     </div>
   )
 }
