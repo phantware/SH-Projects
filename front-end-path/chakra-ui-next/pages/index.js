1517ca33cc33439ee16f54277b075bc2b5339a42
phantware <phantmoney2011@gmail.com>
2022-12-13 22:07:49 +0100

    useColorModeValue utilized

--- front-end-path/chakra-ui-next/pages/index.js
+++ front-end-path/chakra-ui-next/pages/index.js
@@ -1,16 +1,19 @@
-import { Text, Heading, Flex, Stack } from '@chakra-ui/react'
+import { Text, Heading, Flex, Stack, useColorModeValue } from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import DarkModeSwitch from '../components/DarkModeSwitch'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const bg = useColorModeValue('red.500', 'red.200')
+  const color = useColorModeValue('gray.800', 'white')
+
   return (
     <Stack as='main' align='center'>
       <Flex flexDirection='column' maxWidth='700px'>
         <Flex flexDirection='row' w='700px' pt={4} justify='space-between'>
           <DarkModeSwitch />
-          <Text>Home</Text>
+          <Text color={color}>Home</Text>
         </Flex>
         <Heading as='h1' size='2xl' fontWeight='normal'>
           Hello World
