38fb07b5ecee91b5f3ff47e7ad80356c5487e79c
phantware <phantmoney2011@gmail.com>
2022-12-17 16:02:06 +0100

    optional props component explained

--- front-end-path/react-typescript-app/src/components/Greet.tsx
+++ front-end-path/react-typescript-app/src/components/Greet.tsx
@@ -1,18 +1,17 @@
-import { Http2ServerRequest } from 'http2'
-
 type GreetProps = {
   name: String
-  messageCount: number
+  messageCount?: number
   isLoggedIn: boolean
 }
 
 const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props
   return (
     <div>
       {props.isLoggedIn ? (
         <>
           <h2>Hi {props.name}!, Welcome to React-Typescript Tutorial</h2>
-          <p>You have {props.messageCount} new messages</p>
+          <p>You have {messageCount} new messages</p>
         </>
       ) : (
         <h2>Welcome Guest</h2>
