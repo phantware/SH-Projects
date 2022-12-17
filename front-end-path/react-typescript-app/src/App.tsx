8c89be2895d19259fb875c2496a9d21eb2b97ce6
phantware <phantmoney2011@gmail.com>
2022-12-17 15:20:17 +0100

    Status component used to explain passing status in typescript

--- front-end-path/react-typescript-app/src/App.tsx
+++ front-end-path/react-typescript-app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Greet from './components/Greet'
 import Person from './components/Person'
 import PersonList from './components/PersonList'
+import Status from './components/Status'
 
 function App() {
   const personName = {
@@ -28,6 +29,7 @@ function App() {
       <Greet name='Jamie' messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='loading' />
     </div>
   )
 }
