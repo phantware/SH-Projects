e73a2d59eb230b2a7d12b179ca013a83a3367af0
phantware <phantmoney2011@gmail.com>
2022-12-19 12:07:30 +0100

    useReducer in typescript explained

---
+++ front-end-path/react-typescript-app/src/state/Counter.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useReducer } from 'react'
+
+type CounterState = {
+  count: number
+}
+
+type CounterAction = {
+  type: string
+  payload: number
+}
+
+const initialState = {
+  count: 0,
+}
+
+const reducer = (state: CounterState, action: CounterAction) => {
+  switch (action.type) {
+    case 'Increment':
+      return { count: state.count + action.payload }
+    case 'Decrement':
+      return { count: state.count - action.payload }
+    default:
+      return state
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'Increment', payload: 10 })}>
+        Increment 10
+      </button>
+      <button onClick={() => dispatch({ type: 'Decrement', payload: 10 })}>
+        Decrement 10
+      </button>
+    </div>
+  )
+}
+
+export default Counter
